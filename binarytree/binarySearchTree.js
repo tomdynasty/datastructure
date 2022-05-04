d6b3c99e8a21fcd12ed84c1bb850d1fd96e76cfd
tom_chen <tom_chen@mail.chungyo.net>
2022-05-04 21:37:05 +0800

    add binary search tree template

---
+++ binarytree/binarySearchTree.js
@@ -0,0 +1,21 @@
+import Node from './node.js';
+
+class BinarySearchTree {
+  constructor() {
+    // root of a binary search tree
+    this.root = null;
+  }
+    // function to be implemented
+    // insert (data)
+    // remove(data)
+                 
+ 
+    // Helper function
+    // findMinNode()
+    // getRootNode()
+    // inOrder(node)
+    // preOrder(node)
+    // postOrder(node)
+    // search(node, data)
+
+}
