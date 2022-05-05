42382de15b68ae4a13fd5fff0a2933e37b4b3a5b
tom_chen <tom_chen@mail.chungyo.net>
2022-05-05 08:37:28 +0800

    add insert node func at binarytree

--- binarytree/binarySearchTree.js
+++ binarytree/binarySearchTree.js
@@ -1,12 +1,52 @@
 import Node from './node.js';
 
-class BinarySearchTree {
+export default class BinarySearchTree {
   constructor() {
     // root of a binary search tree
     this.root = null;
   }
     // function to be implemented
-    // insert (data)
+    insert (data) {
+      const newNode = new Node(data);
+
+      // root is null then node will
+      // be added to the tree and made root.
+      if (this.root === null) {
+        this.root = newNode;
+      } else {
+        // find the correct position in the
+        // tree and add the node
+        this.insertNode(this.root, newNode);
+      }
+    }
+
+    // Method to insert a node in a tree
+    // it moves over the tree to find the location
+    // to insert a node with a given data
+    insertNode(node, newNode) {
+       // if the data is less than the node
+       // data move left of the tree
+       if (newNode.data < node.data) {
+         if (node.left === null) {
+           node.left = newNode;
+         } else {
+           // if left is not null recur until
+           // null is found
+           this.insertNode(node.left, newNode);
+         }
+       }
+       // if the data is more than the node
+       // data move right of the tree
+       else {
+          if(node.right === null) {
+            node.right = newNode
+          } else {
+            this.insertNode(node.right, newNode);
+          }
+       }
+    }
+
+
     // remove(data)
                  
  
---
+++ binarytree/index.js
@@ -0,0 +1,19 @@
+import BinarySearchTree from './binarySearchTree.js';
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(20);
+tree.insert(18);
+tree.insert(22);
+tree.insert(16);
+
+console.log(tree.root);
+// Node {
+//   data: 10,
+//   left: null,
+//   right: Node {
+//     data: 20,
+//     left: Node { data: 18, left: [Node], right: null },
+//     right: Node { data: 22, left: null, right: null }
+//   }
+// }
