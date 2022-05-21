4405516d4f7b9bcf3d9857ec2df3d3f8ce31a216
tom_chen <tom_chen@mail.chungyo.net>
2022-05-21 09:40:34 +0800

    add enqueue method at priority queue

--- priority_queue/PriorityQueue.js
+++ priority_queue/PriorityQueue.js
@@ -11,6 +11,22 @@ class PriorityQueue {
     }
 
     // functions to be implemented
+    enqueue(element, priority) {
+        const qElement = new QElement(element, priority);
+        let contain = false;
+
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].priority > qElement.priority) {
+                this.items.splice(i, 0, qElement);
+                contain = true;
+                break;
+            }
+        }
+
+        if (!contain) {
+            this.items.push(qElement);
+        }
+    }
     // enqueue(item, priority)
     // dequeue()
     // front()
