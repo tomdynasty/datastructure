ebeba13f08c365f2211c325e085ea665d2919669
tom_chen <tom_chen@mail.chungyo.net>
2022-05-21 09:31:27 +0800

    add priority queue template

---
+++ priority_queue/PriorityQueue.js
@@ -0,0 +1,19 @@
+class QElement {
+    constructor(element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.items = [];
+    }
+
+    // functions to be implemented
+    // enqueue(item, priority)
+    // dequeue()
+    // front()
+    // isEmpty()
+    // printPQueue()  
+}
