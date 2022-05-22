9f98759a5bfac68c5b858889f35ef5a45876e14b
tom_chen <tom_chen@mail.chungyo.net>
2022-05-22 15:59:57 +0800

    add insertion sort

---
+++ sort/insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+      let j = i - 1;
+      const current = arr[i];
+      for (;j >= 0 && arr[j] > current; j--) {
+          // if inner arr[j] is greater than arr[i](current)
+          // then shift arr[j] value to the right
+          arr[j + 1]  = arr[j];
+      }
+      // insert current at correct position 
+      arr[j+1] = current;
+  }
+  return arr;
+}
+
+console.log(insertionSort([5, 3, 6, 1, 4])); // [1, 3, 4, 5, 6]
