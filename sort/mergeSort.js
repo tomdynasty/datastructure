4f53fb8f27515c024a14f695b63664d28b426d7a
tom_chen <tom_chen@mail.chungyo.net>
2022-05-23 21:53:17 +0800

    add merge sort

---
+++ sort/mergeSort.js
@@ -0,0 +1,39 @@
+function mergeSortedArr(leftArr, rightArr) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const leftElement = leftArr[leftIndex];
+        const rightElement = rightArr[rightIndex];
+
+        if (leftElement > rightElement) {
+            result.push(rightElement);
+            rightIndex++;
+        } else {
+            result.push(leftElement);
+            leftIndex++;
+        }
+    }
+    return [...result, ...rightArr.slice(rightIndex), ...leftArr.slice(leftIndex)];
+}
+
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middleIndex = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, middleIndex);
+    const rightArr = arr.slice(middleIndex);
+
+    return mergeSortedArr(
+        mergeSort(leftArr),
+        mergeSort(rightArr)
+    )
+}
+
+const arr = [10, 2, 20 , 30, 40, 50, 60, 7, 100, 99, 7];
+const result = mergeSort(arr);
+// 2 7 7 10 20 30 40 50 60 99 100
+console.log(result);
