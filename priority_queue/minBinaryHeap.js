7e5177bfc63e980b4f15b7af0fd8e37206b8813a
tom_chen <tom_chen@mail.chungyo.net>
2022-06-03 15:53:17 +0800

    add min heap insert func

--- priority_queue/minBinaryHeap.js
+++ priority_queue/minBinaryHeap.js
@@ -7,17 +7,47 @@
 // Operations
 // 1. Insert element
 // 2. Extract element
-export default function MinnBinaryHeap(arr) {
+export default class MinnBinaryHeap {
     constructor(selector) {
         this.items = [];
         this.selector = selector;
     }
 
-    push() {
+    insert(item) {
+        let i = this.items.length;
+        this.items.push(item)
+        let parentIndex = Math.floor(((i + 1) / 2) - 1);
+        if (parentIndex < 0) parentIndex = 0;
 
+        let parentVal = this.selector(this.items[parentIndex]);
+        const pushedVal = this.selector(this.items[i]);
+
+        while (i > 0 && parentVal > pushedVal) {
+            parentIndex = Math.floor(((i + 1) / 2) - 1);
+            this.swap(i, parentIndex);
+            i = parentIndex;
+
+            parentVal = this.selector(
+                this.items[Math.max(Math.floor((i + 1) / 2 - 1), 0)]
+            );
+        }
     }
 
-    pop() {
-        
+    swap(index1, index2){
+        let temp = this.items[index1];
+        this.items[index1] = this.items[index2];
+        this.items[index2] = temp;
+        return this.items;
     }
 }
+
+const selector = (val) => {
+    return val;
+}
+const minHeap = new MinnBinaryHeap(selector);
+minHeap.insert(10);
+minHeap.insert(3);
+minHeap.insert(2);
+minHeap.insert(1);
+// 1 10
+console.log(minHeap);
